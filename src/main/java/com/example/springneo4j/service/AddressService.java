190e8dbfdbf83cf67c66e682fa9fbbaf92adf489
git4shashikant <shashikant.personal@gmail.com>
2022-10-20 13:27:36 +0200

    read transaction.

--- src/main/java/com/example/springneo4j/service/AddressService.java
+++ src/main/java/com/example/springneo4j/service/AddressService.java
@@ -3,10 +3,13 @@ package com.example.springneo4j.service;
 import com.example.springneo4j.entity.Address;
 import com.example.springneo4j.entity.Person;
 
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 public interface AddressService {
     Address saveAddress(Address address);
     Address addResident(Long addressId, Long personId);
     Set<Person> findResidents(Long addressId);
+    List<Map<String, Object>> findPersonFromSameCountry(String country);
 }
--- src/main/java/com/example/springneo4j/service/impl/AddressServiceImpl.java
+++ src/main/java/com/example/springneo4j/service/impl/AddressServiceImpl.java
@@ -5,21 +5,34 @@ import com.example.springneo4j.entity.Person;
 import com.example.springneo4j.repository.AddressRepository;
 import com.example.springneo4j.repository.PersonRepository;
 import com.example.springneo4j.service.AddressService;
+import org.neo4j.driver.Driver;
+import org.neo4j.driver.Record;
+import org.neo4j.driver.Result;
+import org.neo4j.driver.Value;
+import org.neo4j.driver.Values;
+import org.neo4j.driver.exceptions.Neo4jException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
 
 @Service
 public class AddressServiceImpl implements AddressService {
 
+    private final Driver driver;
     private final AddressRepository addressRepository;
     private final PersonRepository personRepository;
 
     @Autowired
-    public AddressServiceImpl(AddressRepository addressRepository, PersonRepository personRepository) {
+    public AddressServiceImpl(Driver driver,
+                              AddressRepository addressRepository,
+                              PersonRepository personRepository) {
+        this.driver = driver;
         this.addressRepository = addressRepository;
         this.personRepository = personRepository;
     }
@@ -41,10 +54,10 @@ public class AddressServiceImpl implements AddressService {
             addressEntity = addressEntityOptional.get();
             personEntity = personEntityOptional.get();
 
-            //addressEntity.addResident(personEntity);
+            addressEntity.addResident(personEntity);
             personEntity.livesAt(addressEntity);
 
-            //addressRepository.save(addressEntity);
+            addressRepository.save(addressEntity);
             personRepository.save(personEntity);
         }
 
@@ -58,5 +71,27 @@ public class AddressServiceImpl implements AddressService {
         addressEntityOptional.ifPresent(address -> residents.addAll(address.getResidents()));
 
         return residents;
+    }
+
+    @Override
+    public List<Map<String, Object>> findPersonFromSameCountry(String country) {
+        List<Map<String, Object>> response = new ArrayList<>();
+        try (var session = driver.session()) {
+            var records = session.readTransaction(tx ->
+                    tx.run("MATCH (p:Person) -[livesAt:LIVES_AT] ->(a:Address) where a.country = $country return p",
+                            Values.parameters("country", country)).list());
+
+            for (Record record : records) {
+                for (Value value : record.values()) {
+                    var node = value.asNode();
+                    response.add(node.asMap());
+                }
+            }
+
+        } catch(Neo4jException neo4jException) {
+            throw neo4jException;
+        }
+
+        return response;
     }
 }
