984eeff909b356276e0ab0c39fa0bc22e8418bef
shashikant.sharma <shashikant.sharma@hm.com>
2022-10-20 13:27:36 +0200

    read transaction.

--- src/main/java/com/example/springneo4j/controller/WebController.java
+++ src/main/java/com/example/springneo4j/controller/WebController.java
@@ -4,6 +4,7 @@ import com.example.springneo4j.entity.Address;
 import com.example.springneo4j.entity.Person;
 import com.example.springneo4j.service.AddressService;
 import com.example.springneo4j.service.PersonService;
+import org.neo4j.driver.Result;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -92,6 +93,11 @@ public class WebController {
     @GetMapping("/address/get-residents/{addressId}")
     public Set<Person> findAddressResidents(@PathVariable(value="addressId") String addressId) {
         return addressService.findResidents(Long.parseLong(addressId));
+    }
+
+    @GetMapping("/address/{country}/get-residents")
+    public List<Map<String, Object>> findAddressResidentsFromSameCountry(@PathVariable(value="country") String country) {
+        return addressService.findPersonFromSameCountry(country);
     }
 
 }
--- src/main/java/com/example/springneo4j/repository/AddressRepository.java
+++ src/main/java/com/example/springneo4j/repository/AddressRepository.java
@@ -1,9 +1,14 @@
 package com.example.springneo4j.repository;
 
 import com.example.springneo4j.entity.Address;
+import org.neo4j.driver.Result;
 import org.springframework.data.neo4j.repository.Neo4jRepository;
+import org.springframework.data.neo4j.repository.query.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AddressRepository extends Neo4jRepository<Address, Long> {
+    @Query("MATCH (p:Person) -[livesAt:LIVES_AT] ->(a:Address) where a.country = $country return p")
+    Result findPersonFromSameCountry(@Param("country") String country);
 }
